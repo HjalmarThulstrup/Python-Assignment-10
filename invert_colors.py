da4d67ba3f3a7eabe7e4ab4b6d4d116c42c1a6a8
unknown <hjalmar_thulstrup@hotmail.com>
2019-04-28 22:14:08 +0200

    finished. Picture is now negative.

--- invert_colors.py
+++ invert_colors.py
@@ -22,17 +22,17 @@ def inverte(imagem, name):
     imagem = (255-imagem)
     cv2.imwrite(name, imagem)
 
-def inverte2(imagem, name):
-    for x in np.nditer(imagem, op_flags=['readwrite']):
-        x = abs(x - 255)
-    cv2.imwrite(name, imagem)
+# def inverte2(imagem, name):
+#     for x in np.nditer(imagem, op_flags=['readwrite']):
+#         x = abs(x - 255)
+#     cv2.imwrite(name, imagem)
 
 
 if __name__ == '__main__':
     img_url = "https://images.unsplash.com/photo-1556162748-a6ae062a7fa3"
     fn = download_image(img_url)
     image = cv2.imread(fn)
-    gs_imagem = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gs_imagem = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     inverte(gs_imagem, fn)
-    inverte2(gs_imagem, fn)
+    #inverte2(gs_imagem, fn)
 
